af2cd04a269cb4bbd6641311dda13331f87a47d5
Basha2120 <bashara2272@gmail.com>
2026-01-13 21:51:50 +0530

    Adding Skil Page Context

--- src/context/SkillContext.jsx
+++ src/context/SkillContext.jsx
@@ -27,6 +27,85 @@ export const SkillProvider = ({ children }) => {
     localStorage.setItem("skills", JSON.stringify(skills));
   }, [skills]);
 
+  const addSkill = (name, category) => {
+    const newSkill = {
+      id: name.toLowerCase().replaceAll(/\s+/g, "-"),
+      name,
+      category,
+      topics: [],
+    };
+
+    setSkills((prev) => [...prev, newSkill]);
+  };
+
+  const addTopic = (skillId, topicName) => {
+    setSkills((prevSkills) =>
+      prevSkills.map((skill) =>
+        skill.id === skillId
+          ? {
+              ...skill,
+              topics: [
+                ...skill.topics,
+                {
+                  id: Date.now(),
+                  name: topicName,
+                  completed: false,
+                },
+              ],
+            }
+          : skill
+      )
+    );
+  };
+
+  const editSkill = (skillId, updatedName, updatedCategory) => {
+    setSkills((prevSkills) =>
+      prevSkills.map((skill) =>
+        skill.id === skillId
+          ? {
+              ...skill,
+              name: updatedName,
+              category: updatedCategory,
+            }
+          : skill
+      )
+    );
+  };
+
+  const deleteSkill = (skillId) => {
+    setSkills((prevSkills) =>
+      prevSkills.filter((skill) => skill.id !== skillId)
+    );
+  };
+
+  const editTopic = (skillId, topicId, updatedName) => {
+    setSkills((prevSkills) =>
+      prevSkills.map((skill) =>
+        skill.id === skillId
+          ? {
+              ...skill,
+              topics: skill.topics.map((topic) =>
+                topic.id === topicId ? { ...topic, name: updatedName } : topic
+              ),
+            }
+          : skill
+      )
+    );
+  };
+
+  const deleteTopic = (skillId, topicId) => {
+    setSkills((prevSkills) =>
+      prevSkills.map((skill) =>
+        skill.id === skillId
+          ? {
+              ...skill,
+              topics: skill.topics.filter((topic) => topic.id !== topicId),
+            }
+          : skill
+      )
+    );
+  };
+
   const toggleTopic = (skillId, topicId) => {
     setSkills((prevSkills) =>
       prevSkills.map((skill) =>
@@ -45,7 +124,18 @@ export const SkillProvider = ({ children }) => {
   };
 
   return (
-    <SkillContext.Provider value={{ skills, toggleTopic }}>
+    <SkillContext.Provider
+      value={{
+        skills,
+        toggleTopic,
+        addSkill,
+        addTopic,
+        editSkill,
+        deleteSkill,
+        editTopic,
+        deleteTopic,
+      }}
+    >
       {children}
     </SkillContext.Provider>
   );
